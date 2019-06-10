f26f5848fe0de19aa3daa0ff1efec1fb7591ad9b
Alexandre Melo <asmelo10@hotmail.com>
2019-06-10 16:40:49 -0300

    Altera o host do backend

--- config/environment.js
+++ config/environment.js
@@ -36,7 +36,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;    
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
   if (environment === 'test') {
@@ -51,7 +51,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.APP.host = 'http://54.207.40.169:8080';
   }
 
   ENV.APP.host = 'http://localhost:8080';
