5808a275f3a20917660cc09463f7414ee9bc2ce7
Alexandre Melo <asmelo10@hotmail.com>
2019-06-10 16:02:50 -0300

    Define rota de acesso ao backend

--- config/environment.js
+++ config/environment.js
@@ -36,8 +36,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
-    ENV.APP.host = 'http://localhost:8080';
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;    
   }
 
   if (environment === 'test') {
@@ -52,10 +51,10 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.APP.host = 'http://54.207.40.169:8080';
+
   }
 
-
+  ENV.APP.host = 'http://localhost:8080';
 
   return ENV;
 };
